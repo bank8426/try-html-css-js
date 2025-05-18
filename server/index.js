cfba0b99a0b8ee9609ea93d21902929b4119d095
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-05-18 14:27:50 +0700

    use express instead of http, try create api GET POST PATCH PUT DLETE

--- server/index.js
+++ server/index.js
@@ -1,15 +1,117 @@
-console.log("hello");
-const http = require("http");
-
-const host = "localhost";
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
 const port = 8000;
 
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end("First Server");
-};
+let users = [];
+let count = 1;
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+app.get("/users", (req, res) => {
+  let filterUsers = users.map((user) => ({
+    firstname: user.firstname,
+    lastname: user.lastname,
+    fullname: user.firstname + " " + user.lastname,
+    id: user.id,
+  }));
+  res.json(filterUsers);
+});
+
+app.get("/users/:id", (req, res) => {
+  let id = req.params.id;
+
+  let foundIndex = findUser(id);
+
+  console.log(foundIndex);
+
+  if (foundIndex === -1) {
+    return res.send({ error: "not found user with id : ", id });
+  }
+
+  res.json({
+    user: users[foundIndex],
+  });
+});
+
+app.post("/users", (req, res) => {
+  let user = req.body;
+  console.log("user");
+  console.log(user);
+  user.id = count;
+  users.push(user);
+  count++;
+
+  console.log("users");
+  console.log(users);
+
+  res.json({
+    message: "success",
+    user,
+  });
+});
+
+app.put("/users/:id", (req, res) => {
+  let id = req.params.id;
+  let user = req.body;
+
+  let foundIndex = findUser(id);
+
+  console.log(foundIndex);
+
+  if (foundIndex === -1) {
+    return res.send({ error: "not found user with id : ", id });
+  }
+
+  users[foundIndex].firstname = user.firstname || users[foundIndex].firstname;
+  users[foundIndex].lastname = user.lastname || users[foundIndex].lastname;
+  users[foundIndex].age = user.age || users[foundIndex].age;
+
+  res.json({
+    message: `update user id ${id} successfully`,
+    user: users[foundIndex],
+  });
+});
+
+app.patch("/users/:id", (req, res) => {
+  let id = req.params.id;
+  let user = req.body;
+
+  let foundIndex = findUser(id);
+
+  if (foundIndex === -1) {
+    return res.send({ error: "not found user with id : ", id });
+  }
+
+  if (user.firstname) users[foundIndex].firstname = user.firstname;
+  if (user.lastname) users[foundIndex].lastname = user.lastname;
+  if (user.age) users[foundIndex].age = user.age;
+
+  res.json({
+    message: `patch user id ${id} successfully`,
+    user: users[foundIndex],
+  });
+});
+
+app.delete("/users/:id", (req, res) => {
+  let id = req.params.id;
+  let foundIndex = findUser(id);
+
+  if (foundIndex === -1) {
+    return res.send({ error: "not found user with id : ", id });
+  }
+
+  let foundUser = users.splice(foundIndex, 1);
+
+  res.json({
+    message: `delete user id ${id} successfully`,
+    user: foundUser,
+  });
+});
+
+function findUser(id) {
+  return users.findIndex((user) => user.id == id);
+}
+
+app.listen(port, (req, res) => {
+  console.log("http server run at " + port);
 });
