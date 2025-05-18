519997bf4286e3950f3652441c4f892357c6c6fb
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-05-18 22:27:36 +0700

    handle edit data

--- try-html-css/index.js
+++ try-html-css/index.js
@@ -1,10 +1,55 @@
 console.log("helllooooo");
+const BASE_URL = "http://localhost:8000";
+
+let mode = "CREATE";
+let selectedId = "";
 
 let firstnameDOM = document.querySelector("input[name=firstname]");
 let lastnameDOM = document.querySelector("input[name=lastname]");
 let ageDOM = document.querySelector("input[name=age]");
 let feedbackDOM = document.querySelector("textarea[name=feedback]");
 let messageDOM = document.getElementById("message");
+
+window.onload = async () => {
+  let urlParams = new URLSearchParams(window.location.search);
+  console.log(urlParams.get("id"));
+  console.log(window.location.search);
+  selectedId = urlParams.get("id");
+  if (selectedId) {
+    mode = "EDIT";
+
+    try {
+      let gendersDOM = document.querySelectorAll("input[name=gender]");
+      let interestsDOM = document.querySelectorAll("input[name=interest]");
+      let response = await axios.get(`${BASE_URL}/users/${selectedId}`);
+      let user = response.data;
+      console.log(response);
+      console.log(response.data);
+
+      firstnameDOM.value = user.firstname;
+      lastnameDOM.value = user.lastname;
+      ageDOM.value = user.age;
+      feedbackDOM.value = user.feedback;
+
+      console.log(gendersDOM);
+
+      for (let i = 0; i < gendersDOM.length; i++) {
+        if (gendersDOM[i].value == user.gender) {
+          gendersDOM[i].checked = true;
+          break;
+        }
+      }
+
+      for (let i = 0; i < interestsDOM.length; i++) {
+        if (user.interest.includes(interestsDOM[i].value)) {
+          interestsDOM[i].checked = true;
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
 
 const validateData = (user) => {
   let errors = [];
@@ -62,15 +107,25 @@ const submitForm = async () => {
     console.log("validateResult");
     console.log(validateResult);
 
-    let respond = await axios.post("http://localhost:8000/users", user);
-    console.log("respond");
-    console.log(respond);
+    if (mode == "CREATE") {
+      let response = await axios.post(`${BASE_URL}/users`, user);
+      console.log("response");
+      console.log(response);
 
-    messageDOM.innerText = "Add new user successful";
-    messageDOM.className = "message success";
+      messageDOM.innerText = "Add new user successful";
+      messageDOM.className = "message success";
+    } else if (mode == "EDIT") {
+      let response = await axios.put(`${BASE_URL}/users/${selectedId}`, user);
+      console.log("response");
+      console.log(response);
+
+      messageDOM.innerText = "Edit user successful";
+      messageDOM.className = "message success";
+    }
   } catch (error) {
     messageDOM.className = "message danger";
-    messageDOM.innerText = "Fail to add new user";
+    messageDOM.innerText =
+      mode == "CREATE" ? "Fail to add new user" : "Fail to edit user";
 
     console.log(error);
 
